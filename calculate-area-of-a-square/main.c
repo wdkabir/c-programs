f81862b80d5db66fefbdbb60b82a4d4af20c1a12
wdkabir <wdkabirhossain@gmail.com>
2023-01-31 23:57:16 +0600

    first commit

---
+++ calculate-area-of-a-square/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+//Write a program to calculate area of a square?
+int main()
+{
+    int side;
+    printf("enter side ");
+    scanf("%d",&side);
+    printf("Total area is : %d", side * side);
+    return 0;
+}
